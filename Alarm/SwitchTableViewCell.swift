037fd6f4f21e75e011d3a31b49ad90b9413ab431
kpfister <kpfister@me.com>
2016-05-16 14:29:29 -0600

    First Commit

---
+++ Alarm/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Karl Pfister on 5/16/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
